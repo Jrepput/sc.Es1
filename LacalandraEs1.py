7d0ac9ff65a46083774882173235fc88c1a488f3
Gabriele Lacalandra <jrepputgl@gmail.com>
2021-01-28 16:26:54 +0100

    Add files via upload

---
+++ LacalandraEs1.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Maiusc+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+  #  print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+ #   print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# code from here:
+
+nm = input("Inserisci il tuo nome: ");
+cg = input('Inserisci il tuo cognome: ');
+an = input("Inserisci l'anno di nascita: ");
+tm = input("Inserisci la temperatura: ");
+
+an = int(an);
+
+ad = an+18;
+
+ad = str(ad);
+
+tm = float(tm);
+
+tm = tm-3.5;
+
+print('Nome:' + nm);
+print('Cognome: ' + cg);
+print('Anno dei diciotto:' + ad);
+print('Temperature: ' + tm);
+
